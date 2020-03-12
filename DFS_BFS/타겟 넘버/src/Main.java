b461f72b8be1ae4d207f592ffc7493e8a447b135
이태윤 <tyleeg0901@gmail.com>
2020-03-12 22:36:34 +0900

    add 프로그래머스/깊이_너비 우선탐색/타겟 넘버 level-2

---
+++ DFS_BFS/타겟 넘버/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Main {
+
+    // test case
+    static int [] numbers = {1, 1, 1, 1, 1};
+    static int target = 3;
+
+    // algorithm solution
+    static class Solution {
+        static int answer=0;
+        static int cnt=0;
+        public int solution(int[] numbers, int target) {
+            int sum=0;
+            dfs(1,numbers,0,target,sum);
+            dfs(0,numbers,0,target,sum);
+            return answer/2;
+        }
+
+        public void dfs(int cal, int []numbers, int idx,int target, int sum){
+
+            if(idx >= numbers.length) {
+                if (sum == target) {
+                    answer++;
+                }
+            }
+            else {
+                if(cal == 1)
+                    sum += numbers[idx];
+                else
+                    sum += -numbers[idx];
+                idx += 1;
+                dfs(1,numbers,idx,target, sum);
+                dfs(0,numbers,idx,target, sum);
+
+            }
+        }
+    }
+
+    // result
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int answer = solution.solution(numbers, target);
+        System.out.println(answer);
+    }
+}
