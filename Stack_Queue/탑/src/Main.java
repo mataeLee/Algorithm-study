e8876f7b4de42ad70ab5192bba50ca04e4270bfa
이태윤 <tyleeg0901@gmail.com>
2020-03-12 19:26:21 +0900

    add 프로그래머스/스택_큐/탑 level-2

---
+++ Stack_Queue/탑/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    // test case
+    static int [] heights = {3,9,9,3,5,7,2};
+
+    static class Solution {
+        public int[] solution(int[] heights) {
+            int[] answer = new int[heights.length];
+
+            for(int i=0; i<heights.length; i++){
+                for(int j=i; j>=0; j--){
+                    if(heights[i] < heights[j]){
+                        answer[i] = j+1;
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int [] answer = solution.solution(heights);
+        for(int i=0; i<answer.length; i++){
+            System.out.print(answer[i] + ", ");
+        }
+    }
+}
