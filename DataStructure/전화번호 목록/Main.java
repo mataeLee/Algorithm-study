e3cf970ebb07b66e471297f19cf824e7ac11c4c5
이태윤 <tyleeg0901@gmail.com>
2021-10-06 22:27:58 +0900

    add 백준/자료구조/전화번호 목록 - 5052

---
+++ DataStructure/전화번호 목록/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // 백준 자료구조 전화번호 목록 - 5052
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            int m = Integer.parseInt(br.readLine());
+            String[] arr = new String[m];
+            for(int j=0; j<m; j++){
+                arr[j] = br.readLine();
+            }
+            Arrays.sort(arr);
+            boolean cons = false;
+            if(arr.length == 1) sb.append("YES");
+            for(int j=1; j<m; j++){
+                String str = arr[j-1];
+                if(arr[j].indexOf(str) == 0) {
+                    sb.append("NO");
+                    cons = true;
+                    break;
+                }
+            }
+            if(!cons) sb.append("YES");
+            sb.append("\n");
+
+        }
+        System.out.println(sb);
+    }
+}
