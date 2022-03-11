cbf0ca57ed8cfc61e27e1b09b7b2d65da3ae7d9b
이태윤 <tyleeg0901@gmail.com>
2022-03-11 17:21:03 +0900

    update 백준/그리디/빵집 - 3109
    reduce memories

--- Greedy/빵집/Main.java
+++ Greedy/빵집/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 백준 그래프 빵집 - 3109
+    // 백준 그리디 빵집 - 3109
 
     static int answer = 0;
     public static void main(String[] args) throws Exception {
@@ -13,31 +13,29 @@ public class Main {
         int C = Integer.parseInt(strs[1]);
 
         int[][] map = new int[R][C];
-        boolean[][] visited = new boolean[R][C];
 
         for(int i=0; i<R; i++){
             String str = br.readLine();
             for(int j=0; j<C; j++){
                 if(str.charAt(j) == 'x') {
                     map[i][j] = 1;
-                    visited[i][j] = true;
                 }
             }
         }
 
         for(int i=0; i<R; i++){
-            answer += dfs(i, 0, map, visited);
+            answer += dfs(i, 0, map);
         }
         System.out.println(answer);
     }
 
-    static int dfs(int i, int j, int[][] map, boolean[][] visited){
+    static int dfs(int i, int j, int[][] map){
         if(i < 0 || i == map.length || j == map[0].length || map[i][j] == 1) return 0;
         map[i][j] = 1;
         if(j == map[0].length -1) return 1;
-        if(dfs(i-1, j+1, map, visited)>0) return 1;
-        else if(dfs(i, j+1, map, visited)>0) return 1;
-        else if(dfs(i+1, j+1, map, visited)>0) return 1;
+        if(dfs(i-1, j+1, map)>0) return 1;
+        else if(dfs(i, j+1, map)>0) return 1;
+        else if(dfs(i+1, j+1, map)>0) return 1;
         return 0;
     }
 }
