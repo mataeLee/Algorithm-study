cf1fda45558e7e603c4a189ee097d9c04a55a28c
이태윤 <41102038+mataeLee@users.noreply.github.com>
2023-11-22 13:51:22 +0900

    Update BOJ_2042.java

--- BOJ/BOJ_2042.java
+++ BOJ/BOJ_2042.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Main{
     // 백준 자료구조 구간 합 구하기 - 2042
     static long[] tree;
-    static long[] arr;
+    
+    static long[] data;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,49 +14,71 @@ public class Main{
         int n = Integer.parseInt(strs[0]);
         int m = Integer.parseInt(strs[1]);
         int k = Integer.parseInt(strs[2]);
+        // 세그먼트 트리
         tree = new long[n*4+1];
-        arr = new long[n];
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<arr.length; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        
+        // 원본 데이터
+        data = new long[n];
+        
+        // input
+        for(int i=0; i<n; i++){
+            data[i] = Long.parseLong(br.readLine());
         }
+        
+        // init
         init(0, n-1, 1);
-        for(int i=0; i<m+k; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
-            if(a == 1){
-                update(0, n-1, 1, b-1, (long)c - arr[b-1]);
-                arr[b-1] = c;
-            }
-            else{
-                sb.append(sum(0, n-1, 1, b-1, (int)c-1)).append("\n");
-            }
+        
+        int count = 0;
+        for(int i=0; i<m+k; i++) {
+        	if(count == k) break;
+        	String[] line = br.readLine().split(" ");
+        	String a = line[0];
+        	String b = line[1];
+        	String c = line[2];
+        	if("1".equals(a)) {
+        		// update
+        		int index = Integer.parseInt(b) - 1;
+        		long diff = Long.parseLong(c) - data[index];
+                // 트리 업데이트
+        		update(0, n-1, 1, index, diff);
+                // 원본 데이터 업데이트
+        		data[index] += diff;
+        	}
+        	else if("2".equals(a)) {
+        		count++;
+        		// sum
+        		long res = sum(0, n-1, 1, Integer.parseInt(b) - 1, Integer.parseInt(c)-1);
+        		System.out.println(res);
+        	}
         }
-
-        System.out.println(sb);
     }
+    
     static long init(int start, int end, int node){
-        if(start == end) return tree[node] = arr[start];
+        if(start == end) return tree[node] = data[start];
+        
         int mid = (start + end) / 2;
-        return tree[node] = init(start, mid, node*2) + init(mid + 1, end, node*2+1);
+        
+        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
     }
-
-    static long sum(int start, int end, int node, int left, int right){
-        if(left > end || right < start) return 0;
-        if(left <= start && end <= right) return tree[node];
-        int mid = (start + end) / 2;
-        return sum(start, mid, node *2, left, right) + sum(mid+1, end, node*2+1, left, right);
+    
+    static long sum(int start, int end, int node, int left, int right) {
+    	if(left > end || right < start) return 0;
+    	
+    	if(left <= start && right >= end) return tree[node];
+    	
+    	int mid = (start + end) / 2;
+    	
+    	return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
     }
-
-    static void update(int start, int end, int node, int index, long dif){
-        if(index < start || index > end) return;
-        tree[node] += dif;
-        if(start == end) return;
-        int mid = (start + end) / 2;
-        update(start, mid, node*2, index, dif);
-        update(mid + 1, end, node*2+1, index, dif);
+    
+    static void update(int start, int end, int node, int index, long diff) {
+    	if(index < start || index > end) return;
+    	
+    	tree[node] += diff;
+    	if(start == end) return;
+    	
+    	int mid = (start + end) / 2;
+    	update(start, mid, node * 2, index, diff);
+    	update(mid + 1 , end, node * 2 + 1, index, diff);       
     }
 }
